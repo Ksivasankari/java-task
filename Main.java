b1d16dd44cca3789460587b53cd980ee9bb34f2a
Ksivasankari <146333643+Ksivasankari@users.noreply.github.com>
2024-11-30 20:14:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+         int[] num=null;
+         try{
+             System.out.println(num[1]);
+         }
+         catch(Exception e){
+             System.out.println(e.getMessage());
+        }
+
+    }
+}
